110af7009131c9b3204541c4a58bdea1a15500be
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2021-02-15 19:19:34 +0100

    feat: add product icon themes and icon themes

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Chameleon
 
-A new random theme every time VSCode starts up.
-Sets the `workbench.colorTheme` by looking at the installed themes.
+Receive a new look every time you open Visual Studio Code:
 
-A new random font every time VSCode starts up.
-Sets the `editor.fontFamily` by shuffling the fonts configured in `editor.fontFamily`.
+- A random theme will be picked from the all of the installed themes, and will be configured in the `workbench.colorTheme` setting
+- Configured fonts from the `editor.fontFamily` setting will be shuffled (comma-separated) in order to use a different font
+- A random icon theme from all of the installed icon themes in the `workbench.iconTheme` setting
+- A random product icon theme from all of the installed product icon themes in the `workbench.productIconTheme` setting
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,82 @@
 import * as vscode from "vscode";
 
-function switchTheme() {
+function switchLook() {
   const userConfig = vscode.workspace.getConfiguration();
 
-  const userThemes: {
+  const colorThemes: {
     label: string;
     uiTheme: string;
     id?: string;
-  }[] =
-    vscode.extensions.all
-      .map((ext) => ext.packageJSON.contributes?.themes || [])
-      .reduce(
-        (allThemes, packageThemes) => [...allThemes, ...packageThemes],
-        []
-      ) || [];
-  const newTheme = userThemes[Math.floor(Math.random() * userThemes.length)];
+  }[] = vscode.extensions.all
+    .map((ext) => ext.packageJSON.contributes?.themes || [])
+    .reduce(
+      (allColorThemes, packageColorThemes) => [
+        ...allColorThemes,
+        ...packageColorThemes,
+      ],
+      []
+    );
+  const colorTheme =
+    colorThemes[Math.floor(Math.random() * colorThemes.length)];
   userConfig.update(
     "workbench.colorTheme",
-    newTheme.id || newTheme.label,
+    colorTheme.id || colorTheme.label,
     true
   );
 
-  const userFont: string = userConfig.get("editor.fontFamily") || "";
-  if (userFont) {
-    const newUserFonts = userFont
+  const fonts: string = userConfig.get("editor.fontFamily") || "";
+  if (fonts) {
+    const font = fonts
       .split(",")
       .map((f) => f.trim())
       .sort(() => 0.5 - Math.random())
       .join(", ");
-    userConfig.update("editor.fontFamily", newUserFonts, true);
+    userConfig.update("editor.fontFamily", font, true);
   }
+
+  const productIconThemes: {
+    label: string;
+    id: string;
+  }[] = [
+    { label: "Default", id: "Default" },
+    ...vscode.extensions.all
+      .map((ext) => ext.packageJSON.contributes?.productIconThemes || [])
+      .reduce(
+        (allProductIcons, packageIcons) => [
+          ...allProductIcons,
+          ...packageIcons,
+        ],
+        []
+      ),
+  ];
+  const productIconTheme =
+    productIconThemes[Math.floor(Math.random() * productIconThemes.length)];
+  userConfig.update(
+    "workbench.productIconTheme",
+    productIconTheme.id || productIconTheme.label,
+    true
+  );
+
+  const iconThemes: {
+    label: string;
+    id: string;
+  }[] = vscode.extensions.all
+    .map((ext) => ext.packageJSON.contributes?.iconThemes || [])
+    .reduce((allIcons, packageIcons) => [...allIcons, ...packageIcons], []);
+  const iconTheme = iconThemes[Math.floor(Math.random() * iconThemes.length)];
+  userConfig.update(
+    "workbench.iconTheme",
+    iconTheme.id || iconTheme.label,
+    true
+  );
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  switchTheme();
+  switchLook();
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("chameleon.switchTheme", () => {
-      switchTheme();
+    vscode.commands.registerCommand("chameleon.switchLook", () => {
+      switchLook();
     })
   );
 }
